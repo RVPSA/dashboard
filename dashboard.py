99d1f3295a6258b12b93052f7e293e53d7372517
Akesh_Ramasinghe <akeshramasinghe@gmail.com>
2022-07-09 10:23:17 +0530

    Update dashboard.py
    modify dashboard two show information of two patients

--- dashboard.py
+++ dashboard.py
@@ -8,6 +8,9 @@ from streamlit_option_menu import option_menu
 firebase = firebase.FirebaseApplication('https://esp32-5c542-default-rtdb.firebaseio.com', None)
 bpm = firebase.get('/test/a', '')
 print(bpm)
+
+bpm2 = firebase.get('/test2/a', '')
+print(bpm2)
 #sometimes, When we run above code firsttime it can propagate and syntax error, to solve that error please
 #rename 'async' file as 'async_' which is in this path C:\Users\mrsak\OneDrive\Desktop\dashboard\venv\Lib\site-packages\firebase
 #also reaname the import location of the async file as async_ inside the firebase and _init_ file which are located
@@ -26,36 +29,80 @@ st.title("Real-Time Medical Dashboard")
 #make empty body
 placeholder = st.empty()
 
-while True:
-    bpm = firebase.get('/test/a', '')
-    spo2 =  firebase.get('/test/b', '') #get values from the databse
+with st.sidebar:
+    selected = option_menu(
+        menu_title= "Main Menu",
+        options = ["Patient_1","Patient_2"]
+    )
 
-    with placeholder.container():
-        kpi1,kpi2 = st.columns(2) #creation of kpi
+if selected == "Patient_1":
+    while True:
+        bpm = firebase.get('/test/a', '')
+        spo2 = firebase.get('/test/b', '')  # get values from the databse
 
-        kpi1.metric(label="BPM1 ⏳", value=bpm)
-        kpi2.metric(label="SPO2 ⏳", value=spo2)
+        with placeholder.container():
+            kpi1, kpi2 = st.columns(2)  # creation of kpi
 
-        gauge1,gauge2 = st.columns(2)
-        g1 = go.Figure(go.Indicator(
-            mode="gauge+number",
-            value=bpm,
-            gauge={
-                'axis': {
-                    'range': [None, 200]
-                }
-            },
-            domain={'x': [0, 1], 'y': [0, 1]},
-            title={'text': "BPM"}))
-        g2 = go.Figure(go.Indicator(
-            mode="gauge+number",
-            value=spo2,
-            gauge={
-                'axis': {
-                    'range': [None, 200]
-                }
-            },
-            domain={'x': [0, 1], 'y': [0, 1]},
-            title={'text': "SPO2"}))
-        gauge1.plotly_chart(g1, use_container_width=True)
-        gauge2.plotly_chart(g2, use_container_width=True)
+            kpi1.metric(label="BPM1 ⏳", value=bpm)
+            kpi2.metric(label="SPO2 ⏳", value=spo2)
+
+            gauge1, gauge2 = st.columns(2)
+            g1 = go.Figure(go.Indicator(
+                mode="gauge+number",
+                value=bpm,
+                gauge={
+                    'axis': {
+                        'range': [None, 200]
+                    }
+                },
+                domain={'x': [0, 1], 'y': [0, 1]},
+                title={'text': "BPM"}))
+            g2 = go.Figure(go.Indicator(
+                mode="gauge+number",
+                value=spo2,
+                gauge={
+                    'axis': {
+                        'range': [None, 200]
+                    }
+                },
+                domain={'x': [0, 1], 'y': [0, 1]},
+                title={'text': "SPO2"}))
+            gauge1.plotly_chart(g1, use_container_width=True)
+            gauge2.plotly_chart(g2, use_container_width=True)
+
+if selected == "Patient_2":
+    while True:
+        bpm2 = firebase.get('/test2/a', '')
+        #spo2 = firebase.get('/test/b', '')  # get values from the databse
+
+        with placeholder.container():
+            kpi1, kpi2 = st.columns(2)  # creation of kpi
+
+            kpi1.metric(label="BPM1 ⏳", value=bpm2)
+            kpi2.metric(label="SPO2 ⏳", value=bpm2)
+
+            gauge1, gauge2 = st.columns(2)
+            g1 = go.Figure(go.Indicator(
+                mode="gauge+number",
+                value=bpm2,
+                gauge={
+                    'axis': {
+                        'range': [None, 200]
+                    }
+                },
+                domain={'x': [0, 1], 'y': [0, 1]},
+                title={'text': "BPM"}))
+            g2 = go.Figure(go.Indicator(
+                mode="gauge+number",
+                value=bpm2,
+                gauge={
+                    'axis': {
+                        'range': [None, 200]
+                    }
+                },
+                domain={'x': [0, 1], 'y': [0, 1]},
+                title={'text': "SPO2"}))
+            gauge1.plotly_chart(g1, use_container_width=True)
+            gauge2.plotly_chart(g2, use_container_width=True)
+
+
